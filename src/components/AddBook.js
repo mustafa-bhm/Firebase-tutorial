b8eaab1d5c2578d292dc52484ab2953f4b3d24a4
mustafa <mustafa.bhm3@gmail.com>
2022-12-12 10:55:03 -0700

    initial commit

---
+++ src/components/AddBook.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import { Form, InputGroup, Button, ButtonGroup } from "react-bootstrap";
+
+function AddBook() {
+  return (
+    <div>
+      <div className="p-4 box">
+        <Form>
+          <Form.Group className="mb-3"></Form.Group>
+        </Form>
+      </div>
+    </div>
+  );
+}
+
+export default AddBook;
