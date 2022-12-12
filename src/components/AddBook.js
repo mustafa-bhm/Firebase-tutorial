b569b6005a801efee83826e16e00ae1004ca222f
mustafa <mustafa.bhm3@gmail.com>
2022-12-12 16:35:24 -0700

    add new book functionality

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,31 +1,94 @@
 import React, { useState } from "react";
-import { Form, InputGroup, Button, ButtonGroup } from "react-bootstrap";
+import { Form, InputGroup, Button, ButtonGroup, Alert } from "react-bootstrap";
+import BookDataService from "../services/book.services";
 
 function AddBook() {
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
   const [status, setStatus] = useState("Available");
   const [flag, setFlag] = useState(true);
+  const [message, setMessage] = useState({ error: false, msg: "" });
+
+  const handlSubmit = async (e) => {
+    e.preventDefault();
+    setMessage("");
+    if (title === "" || author === "") {
+      setMessage({ error: true, msg: "No empty fields are allowed" });
+      return;
+    }
+    const newBook = {
+      title: title,
+      author: author,
+      status: status,
+    };
+
+    // adding the new book to firbase collection
+    try {
+      await BookDataService.addBooks(newBook);
+      setMessage({ error: false, msg: "new book has been added" });
+    } catch (err) {
+      setMessage({ error: true, msg: err.message });
+    }
+
+    setAuthor("");
+    setTitle("");
+  };
+
   return (
     <>
       <div className="p-4 box">
-        <Form>
+        {message?.msg && (
+          <Alert
+            variant={message?.error ? "danger" : "success"}
+            dismissible
+            onClose={() => setMessage("")}
+          >
+            {message?.msg}
+          </Alert>
+        )}
+        <Form onSubmit={handlSubmit}>
           <Form.Group className="mb-3" controlId="formBookTitle">
             <InputGroup>
               <InputGroup.Text id="formBookTitle">B</InputGroup.Text>
-              <Form.Control type="text" placeholder="Book Title" />
+              <Form.Control
+                type="text"
+                placeholder="Book Title"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+              />
             </InputGroup>
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="formBookAuthor">
             <InputGroup>
               <InputGroup.Text id="formBookAuthor">A</InputGroup.Text>
-              <Form.Control type="text" placeholder="Book Author" />
+              <Form.Control
+                type="text"
+                placeholder="Book Author"
+                value={author}
+                onChange={(e) => setAuthor(e.target.value)}
+              />
             </InputGroup>
           </Form.Group>
           <ButtonGroup aria-label="Basic example" className="mb-3">
-            <Button disabled={flag} variant="success">
+            <Button
+              disabled={flag}
+              variant="success"
+              onClick={(e) => {
+                setStatus("Available");
+                setFlag(true);
+              }}
+            >
               Available
             </Button>
-            <Button variant="danger" disabled={!flag}>
+            <Button
+              variant="danger"
+              disabled={!flag}
+              onClick={(e) => {
+                setStatus("Not Available");
+                setFlag(false);
+              }}
+            >
               Not Available
             </Button>
           </ButtonGroup>
